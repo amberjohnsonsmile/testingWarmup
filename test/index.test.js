27df417c3cbb0d50d4399d3e7c3c17770367b6ff
Brian Novak <bn@BMBP.local>
2017-12-04 09:37:19 -0700

    Quick Update

--- test/index.test.js
+++ test/index.test.js
@@ -5,13 +5,10 @@ describe("Array", function() {
 	describe("#sumTwoNum()", function() {
 		it("should return the sum of two numbers", function () {
 			assert.equal(index.sumTwoNum(2,3), 5);
-			assert.equal(index.sumTwoNum(1,9), 10);
-			assert.equal(index.sumTwoNum(3,4), 7);
 		});
 	describe("#findArrayLength()", function(){
 		it("should return length of the array", function(){
 			assert.equal(index.findArrayLength(arr), 6);
-			assert.equal(index.findArrayLength(arr2), 2);
 		});
 	});
 	});
